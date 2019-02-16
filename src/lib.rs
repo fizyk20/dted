a4f639b42655881395fa7cd8640fc2f845fc6e17
Bartłomiej Kamiński <fizyk20@gmail.com>
2019-02-16 16:15:31 +0100

    Add a test

--- src/lib.rs
+++ src/lib.rs
@@ -152,3 +152,18 @@ pub fn read_dted<P: AsRef<Path>>(path: P) -> Result<DtedData, Error> {
     let data = parse_dted_file(&content)?.1;
     Ok(data)
 }
+
+#[test]
+fn test_input_data() {
+    let data = read_dted("test_data/test_data.dt2").unwrap();
+    assert_eq!(data.header.origin_lat.deg, 42);
+    assert_eq!(data.header.origin_lat.min, 0);
+    assert_eq!(data.header.origin_lat.sec, 0);
+    assert_eq!(data.header.origin_lon.deg, 15);
+    assert_eq!(data.header.origin_lon.min, 0);
+    assert_eq!(data.header.origin_lon.sec, 0);
+    assert_eq!(data.header.lat_interval, 10);
+    assert_eq!(data.header.lon_interval, 10);
+    assert_eq!(data.header.num_lat_lines, 3601);
+    assert_eq!(data.header.num_lon_lines, 3601);
+}
