9c32a4b9306d1c1716007079c7dfaeee14ba912b
Bartłomiej Kamiński <fizyk20@gmail.com>
2019-02-16 16:32:03 +0100

    Add conversions of Angle to floats

--- src/data/angle.rs
+++ src/data/angle.rs
@@ -1,5 +1,29 @@
+use std::convert::From;
+
 pub struct Angle {
     pub deg: i16,
     pub min: u8,
     pub sec: u8,
+}
+
+impl From<Angle> for f32 {
+    fn from(ang: Angle) -> f32 {
+        let result = ang.deg.abs() as f32 + (ang.min as f32) / 60.0 + (ang.sec as f32) / 3600.0;
+        if ang.deg < 0 {
+            -result
+        } else {
+            result
+        }
+    }
+}
+
+impl From<Angle> for f64 {
+    fn from(ang: Angle) -> f64 {
+        let result = ang.deg.abs() as f64 + (ang.min as f64) / 60.0 + (ang.sec as f64) / 3600.0;
+        if ang.deg < 0 {
+            -result
+        } else {
+            result
+        }
+    }
 }
